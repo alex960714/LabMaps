f373c4a05443cf0d7c1c812f2feb3e09b657991d
alex960714 <alex_9607@mail.ru>
2017-12-27 20:37:05 +0300

    Added markers handling

--- app/src/main/java/com/example/user/labmaps/MapsActivity.java
+++ app/src/main/java/com/example/user/labmaps/MapsActivity.java
@@ -3,16 +3,24 @@ package com.example.user.labmaps;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button newMarkerBtn;
+    private Button delMarkerBtn;
+    private ArrayList<Marker> markers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        newMarkerBtn = (Button)findViewById(R.id.newMarkerButton);
+
+        markers=new ArrayList<>();
     }
 
 
@@ -38,9 +50,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().position(latLng)
+                        .title(Double.valueOf(latLng.latitude).toString()
+                                + " " + Double.valueOf(latLng.longitude).toString())
+                        .draggable(true));
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                deleteMarker(marker);
+                delButtonInactive();
+                return false;
+            }
+        });
+
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                marker.setTitle(Double.valueOf(marker.getPosition().latitude).toString()
+                        + " "+ Double.valueOf(marker.getPosition().longitude).toString() );
+            }
+        });
+    }
+
+    public void deleteMarker(final Marker marker){
+        delMarkerBtn=(Button)findViewById(R.id.delMarkerButton);
+        delMarkerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                marker.remove();
+            }
+        });
+    }
+
+    public void delButtonInactive(){
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                delMarkerBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+            }
+        });
     }
 }
